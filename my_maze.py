3851fd2dabf8ad680aacd3a1b821557783c605bc
Esther Babun <167118015+EstherBabun@users.noreply.github.com>
2026-01-18 15:34:06 +0100

    adapt the load_config() method to take default values if parsing fails

--- my_maze.py
+++ my_maze.py
@@ -14,7 +14,7 @@ import random
 from typing import Dict, List, Any
 
 
-class Cell(object):
+class Cell:
     """Class that holds the cell attributes in a 2D maze.
 
     Attributes:
@@ -32,21 +32,17 @@ class Cell(object):
         self.walls: Dict[str, int] = {"W": 1, "S": 1, "E": 1, "N": 1}
         self.is_entry: bool = False
         self.is_exit: bool = False
-        self.current: bool = False
         self.visited: bool = False
 
     @property
     def hex_repr(self) -> str:
         """Convert the status of the walls to an hex representation."""
         # store binary representation of walls into a string
-        str_bin: str = "".join(str(v) for v in self.walls.values())
+        binary_str = "".join(str(v) for v in self.walls.values())
 
-        # convert string from binary to decimal with int(str, 2)
-        dec_repr: int = int(str_bin, 2)
-
-        # Convert to hex (without '0x' prefix)
-        hex_repr: str = hex(dec_repr)[2:].upper()
-        return hex_repr
+        # convert string from binary to decimal with int(binary_str, 2)
+        # convert to hex using format specifier :X
+        return f"{int(binary_str, 2):X}"
 
 
 class Maze:
@@ -60,49 +56,68 @@ class Maze:
             }
     adjacent: Dict[str, str] = {"N": "S", "S": "N", "E": "W", "W": "E"}
 
-    def __init__(self) -> None:
+    def __init__(self, config_file: Optional[str] = None) -> None:
         """Initialise the attributes of the maze with the default config."""
-        self.cols: int = 20
-        self.rows: int = 10
+        dict_config: Dict[str, Any] = self.load_config(config_file)
+        self.cols: int = dict_config["WIDTH"]
+        self.rows: int = dict_config["HEIGHT"]
         self.tot_size: int = self.cols * self.rows
-        self.entry: tuple = (0, 0)
-        self.exit: tuple = (self.cols - 1, self.rows - 1)
+        self.entry: tuple = dict_config["ENTRY"]
+        self.exit: tuple = dict_config["EXIT"]
         self.path: str = ""
         self.grid: List[List[Cell]] = [[Cell(x, y) for x in range(self.cols)]
                                        for y in range(self.rows)]
-        self.seed: Union[int, None] = None
-        self.perfect: bool = True
+        self.seed: int | None = dict_config["SEED"]
+        self.perfect: bool = dict_config["PERFECT"]
 
-    def configure_maze(self, config: Dict[str, Any]) -> None:
-        """Initialise the attributes of the maze with the loaded config."""
-        if "WIDTH" in config.keys():
-            self.cols = config["WIDTH"]
-        if "HEIGHT" in config.keys():
-            self.rows = config["HEIGHT"]
-        self.tot_size = self.cols * self.rows
-        if "ENTRY" in config.keys():
-            self.entry = config["ENTRY"]
-        if "EXIT" in config.keys():
-            self.exit = config["EXIT"]
-        if "SEED" in config.keys():
-            self.seed = config["SEED"]
-        if "PERFECT" in config.keys():
-            self.perfect = config["PERFECT"]
+    def print_config(self, dict_config: Dict[str, Any], custom: List[str]) -> None:
+        """Print final settings of the maze."""
+        print("\nMaze configuration:")
+        for k, v in dict_config.items():
+            if k in custom:
+                print(f"  {k}: {v}")
+            else:
+                print(f"  {k}: {v} (default)")
 
     def load_config(self, file: str) -> Dict[str, Any]:
         """Parse the config file and return the key,value pairs."""
-        dict_config: dict[str, Any] = {}
+        # create default dict settings
+        dict_config: dict[str, Any] = {
+                "WIDTH": 20,
+                "HEIGHT": 10,
+                "SEED": None,
+                "PERFECT": True
+                }
+        custom: List[str] = []
+
+        if file is None:
+            print("No config file, switching to default settings.")
+            dict_config["ENTRY"] = (0, 0)
+            dict_config["EXIT"] = (19, 9)
+            self.print_config(dict_config, custom)
+            return dict_config
 
         # read from file and create dict of strings:
         try:
             with open(file, "r") as f:
-                for line in f:
+                content: str = f.read()
+                if content == '':
+                    print(f"Config file is empty - switching to default settings")
+                    dict_config["ENTRY"] = (0, 0)
+                    dict_config["EXIT"] = (19, 9)
+                    self.print_config(dict_config, custom)
+                    return dict_config
+
+                print(f"Loading settings from config file {file}...")
+                for line in content.splitlines():
                     line = line.strip()
                     if line and not line.startswith('#'):
                         key, value = line.split('=', 1)
-                        dict_config[key.strip().upper()] = value.strip()
+                        key = key.strip().upper()
+                        dict_config[key] = value.strip()
+                        custom.append(key)
         except ValueError as e:
-            print(f"Error in line {line}: {e}")
+            print(f'Error in line {line} - Expected syntax: "KEY=value"')
             return {}
         except (FileNotFoundError, PermissionError) as e:
             print(f"Error: {e}")
@@ -113,69 +128,108 @@ class Maze:
 
         # Convert numeric values, tuples and booleans
         for k, v in dict_config.items():
-            try:
-                # Numeric values for WIDTH and HEIGHT
-                if k == "WIDTH" or k == "HEIGHT":
-                    dict_config[k] = int(v)
-                    # Raise error to prevent huge maze:
-                    # if dict_config[k] > MAX_SIZE(tbd) raise error
-                # tuples for ENTRY and EXIT
-                elif k == "ENTRY" or k == "EXIT":
-                    dict_config[k] = tuple(int(i.strip()) for i in v.split(','))
-                    if len(dict_config[k]) > 2:
-                        raise ValueError(f'{k} expects 2 values "x,y"')
-                # bool value for PERFECT
-                elif k == "PERFECT":
-                    if v.upper() == "TRUE":
-                        dict_config[k] = True
-                    elif v.upper() == "FALSE":
-                        dict_config[k] = False
+            if k in custom:
+                try:
+                    # Numeric values for WIDTH and HEIGHT
+                    if k == "WIDTH" or k == "HEIGHT":
+                        dict_config[k] = int(v)
+                        # Raise error to prevent huge maze:
+                        # if dict_config[k] > MAX_SIZE(tbd) raise error
+                    # tuples for ENTRY and EXIT
+                    elif k == "ENTRY" or k == "EXIT":
+                        dict_config[k] = tuple(int(i.strip()) for i in v.split(','))
+                        if len(dict_config[k]) > 2:
+                            raise ValueError(f'{k} expects 2 values "x,y"')
+                    # bool value for PERFECT
+                    elif k == "PERFECT":
+                        if v.upper() == "TRUE":
+                            dict_config[k] = True
+                        elif v.upper() == "FALSE":
+                            dict_config[k] = False
+                        else:
+                            raise ValueError(f'{k} expects "True" or "False"')
+                    # path name of the OUTPUT_FILE
+                    elif k == "OUTPUT_FILE":
+                        dict_config[k] = v
+                    # the chosen seed
+                    elif k == "SEED":
+                        if dict_config["SEED"] == None:
+                            continue
+                        else:
+                            dict_config[k] = int(v)
+                    # discard invalid keys
                     else:
-                        raise ValueError(f'{k} expects "True" or "False"')
-                # path name of the OUTPUT_FILE
-                elif k == "OUTPUT_FILE":
-                    dict_config[k] = v
-                # the chosen seed
-                elif k == "SEED":
-                    dict_config[k] = int(v)
-                # discard invalid keys
-                else:
-                    raise ValueError(
-                            "Expected: WIDTH, HEIGHT, ENTRY, "
-                            "EXIT, OUTPUT_FILE, PERFECT, SEED"
+                        print(
+                                f"Error in {k} - "
+                                "Allowed: WIDTH, HEIGHT, ENTRY, "
+                                "EXIT, OUTPUT_FILE, PERFECT, SEED"
+                                )
+                except Exception as e:
+                    default_value = {
+                        "WIDTH": 20,
+                        "HEIGHT": 10,
+                        "SEED": None,
+                        "PERFECT": True
+                        }.get(k, None)
+
+                    print(
+                            f'Error in {k}: {e}\n'
+                            f'Switching to default {k}'
                             )
-            except Exception as e:
-                print(f"Error in config file: {k}={v}\n{e}")
-                return {}
+                    dict_config[k] = default_value
+                    custom.remove(k)
+
+        w = dict_config["WIDTH"]
+        h = dict_config["HEIGHT"]
+        if "ENTRY" not in custom:
+            dict_config["ENTRY"] = (0, 0)
+        if "EXIT" not in custom:
+            dict_config["EXIT"] = (w - 1, h - 1)
 
         # Check if entry/exit are within bounds
         entry_x, entry_y = dict_config["ENTRY"]
         exit_x, exit_y = dict_config["EXIT"]
-        w = dict_config["WIDTH"]
-        h = dict_config["HEIGHT"]
         if entry_x < 0 or entry_y < 0 or entry_x > w - 1 or entry_y > h - 1:
-            print("Error: Entry point exceeds borders of the maze.")
-            return {}
+            print(
+                    "Error: Entry point exceeds borders of the maze.\n"
+                    'Switching to default entry'
+                    )
+            dict_config["ENTRY"] = (0, 0)
+            custom.remove("ENTRY")
         if exit_x < 0 or exit_y < 0 or exit_x > w - 1 or exit_y > h - 1:
-            print("Error: Exit point exceeds borders of the maze.")
-            return {}
+            print(
+                    "Error: Exit point exceeds borders of the maze.\n"
+                    'Switching to default exit'
+                    )
+            dict_config["EXIT"] = (w - 1, h - 1)
+            custom.remove("EXIT")
+        # Check if entry/exit are within the 42 blocked cells
+        ft_walls: List[tuple] = self.get_42_cells(w, h)
+        if dict_config["ENTRY"] in ft_walls:
+            print(
+                    f"Entry point is stuck in the 42 cells\n"
+                        "Switching to default entry"
+                    )
+            dict_config["ENTRY"] = (0, 0)
+            custom.remove("ENTRY")
 
+        if dict_config["EXIT"] in ft_walls:
+            print(
+                    f"Exit point is stuck in the 42 cells\n"
+                    "Switching to default exit"
+                    )
+            dict_config["EXIT"] = (w - 1, h - 1)
+            custom.remove("EXIT")
+
+        self.print_config(dict_config, custom)
         return dict_config
 
-    def get_42_cells(self) -> List[tuple]:
+    def get_42_cells(self, w: int, h: int) -> List[tuple]:
         """Calculate the coordinates of the 42 cells."""
-        w = self.cols
-        h = self.rows
         if w < 9 or h < 7:
                 return [] #  No 42_walls, maze too small
-        if w % 2 == 0:
-                cx = math.floor(w / 2) - 1
-        else:
-                cx = math.floor(w / 2)
-        if h % 2 == 0:
-                cy = math.floor(h / 2) - 1
-        else:
-                cy = math.floor(h / 2)
+        cx: int = (w - 1) // 2 if w % 2 == 0 else w // 2
+        cy: int = (h - 1) // 2 if h % 2 == 0 else h // 2
 
         four_walls: List[tuple] = [(cx - 1, cy), (cx - 2, cy), (cx - 3, cy),
                         (cx - 1, cy + 1), (cx - 1, cy + 2),
@@ -188,52 +242,34 @@ class Maze:
 
         ft_walls = four_walls + two_walls
         return ft_walls
-
-    def validate_entry_exit(self) -> bool:
-        """Check if entry/exit is outside of the blocked 42 cells."""
-        ft_walls = self.get_42_cells()
-        # Check if entry/exit are in 42 blocked cells
-        if self.entry in ft_walls:
-                print(f"Wrong entry point: {self.entry}")
-                print(f"Forbiden: {ft_walls}")
-                return False #  return false to stop execution !
-        if self.exit in ft_walls:
-                print(f"Wrong exit point: {self.exit}")
-                print(f"Forbiden: {ft_walls}")
-                return False #  return false to stop execution
-
-        return True
-
     
     def block_42_walls(self) -> None:
         """Prevent access to the 42 walls in the center of the maze."""
-        ft_walls: List[tuple] = self.get_42_cells()
-        for item in ft_walls:
-                x, y = item
-                self.grid[y][x].visited = True
+        for x, y in self.get_42_cells(self.cols, self.rows):
+            self.grid[y][x].visited = True
 
-    def get_cell(self, x: int, y: int) -> Optional[Cell]:
+    def get_cell(self, x: int, y: int) -> Cell | None:
         """Get cell at (x, y), return None if out of borders."""
         if 0 <= x < self.cols and 0 <= y < self.rows:
             return self.grid[y][x]
         return None
 
-    def get_unvisited_commons(self, cell: Cell) -> List[tuple]:
+    def get_unvisited_neighbors(self, cell: Cell) -> List[tuple]:
         """get all unvisited cells that have common wall with current cell."""
-        commons: List[tuple] = []
+        neighbors: List[tuple] = []
         x, y = cell.coord
         for direction, (ox, oy) in self.offset.items():
             nx, ny = x + ox, y + oy
-            common: Cell = self.get_cell(nx, ny)
-            if common and not common.visited:
-                commons.append((direction, common))
-        return commons
+            neighbor: Cell = self.get_cell(nx, ny)
+            if neighbor and not neighbor.visited:
+                neighbors.append((direction, neighbor))
+        return neighbors
 
-    def delete_wall(self, cell: Cell, common: Cell, direction: str) -> None:
+    def delete_wall(self, cell: Cell, neighbor: Cell, direction: str) -> None:
         """Delete wall between two cells."""
         cell.walls[direction] = 0
         adj: str = self.adjacent[direction]
-        common.walls[adj] = 0
+        neighbor.walls[adj] = 0
     
     def generate_maze(self) -> bool:
         """Generate maze with dfs backtracking."""
@@ -241,9 +277,7 @@ class Maze:
         entry_x, entry_y = self.entry 
         # block access to 42 walls
         self.block_42_walls()
-        # check entry/exit aren't within blocked cells
-        if self.validate_entry_exit() is False:
-            return False
+
         stack = []
         current = self.get_cell(entry_x, entry_y)
         current.visited = True
@@ -253,9 +287,9 @@ class Maze:
             random.seed(self.seed)
 
         while n_visited < self.tot_size:
-            commons = self.get_unvisited_commons(current)
-            if commons:
-                direction, next_cell = random.choice(commons)
+            neighbors = self.get_unvisited_neighbors(current)
+            if neighbors:
+                direction, next_cell = random.choice(neighbors)
                 self.delete_wall(current, next_cell, direction)
                 stack.append(current)
                 current = next_cell
@@ -275,13 +309,14 @@ class Maze:
         return True
 
 
-    def print_grid_hexa(self):
+    @property
+    def hex_repr(self):
         """To print in hexa the grid of the maze"""
+        maze_hex: str = ""
         for y in range(self.rows):
-            row = ""
-            for x in range(self.cols):
-                row += self.grid[y][x].hex_repr
-            print(row)
+            maze_hex += "".join(self.grid[y][x].hex_repr for x in range(self.cols))
+            maze_hex += "\n"
+        return maze_hex
 
     def print_maze_visual(self):
         """Print a visual ASCII representation of the maze."""
@@ -295,12 +330,7 @@ class Maze:
                 cell = self.grid[y][x]
 
                 # Cell marker (entry/exit)
-                if cell.is_entry:
-                    row += "S"
-                elif cell.is_exit:
-                    row += "E"
-                else:
-                    row += " "
+                row += "S" if cell.is_entry else "E" if cell.is_exit else " "
 
                 # East wall
                 if cell.walls['E']:
@@ -337,27 +367,21 @@ def main() -> None:
     """Docstring to write."""
     # check the arguments
     if len(sys.argv) == 1:
-        print("No config file, switching to default settings.")
         # Init maze with default settings
         my_maze: maze = Maze()
 
     if len(sys.argv) == 2:
-        print(f"Loading settings from config file {sys.argv[1]}")
         # take the path to the config file
         config_file: str = sys.argv[1]
         # create maze instance
-        my_maze: Maze = Maze()
-        # parse config file into a dict
-        config: Dict[str, Any] = my_maze.load_config(config_file)
-        # Stop if error in config file
-        if not config:
+        my_maze: Maze = Maze(config_file)
+        if my_maze is None:
             return
-        # set maze attributes with loaded config
-        my_maze.configure_maze(config)
-
+    # generate maze
     if my_maze.generate_maze() is False:
         return
     my_maze.print_maze_visual()
+    print(my_maze.hex_repr)
     
 
 
