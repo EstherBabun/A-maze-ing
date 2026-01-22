27698c380910f718c0ad476acd61a8661bd53949
Esther Babun <167118015+EstherBabun@users.noreply.github.com>
2026-01-22 10:36:44 +0100

    copy content of merged branch in esther's branch

---
+++ a_maze_ing.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# File: a_maze_ing.py
+# Author: ebabun <ebabun@student.42belgium.be>
+# Author: mmeurer <mmeurer@student.42belgium.be>
+# Created: 2026/01/22 09:44:42
+# Updated: 2026/01/22 09:44:42
+
+"""Docstring to write."""
+
+import sys
+from maze_generator import MazeGenerator
+
+
+def main() -> None:
+    """Docstring to write."""
+    # check the arguments
+    if len(sys.argv) == 1:
+        # Init maze with default settings
+        my_maze: MazeGenerator = MazeGenerator()
+    elif len(sys.argv) == 2:
+        # take the path to the config file
+        config_file: str = sys.argv[1]
+        # create maze instance
+        my_maze: MazeGenerator = MazeGenerator(config_file)
+    else:
+        print("Usage: python3 a_maze_ing.py config_file(optional)")
+        return
+
+    # generate maze passing "DFS" or "Wilson" as argument
+    my_maze.generate_maze()
+    my_maze.print_maze_visual()
+
+
+if __name__ == "__main__":
+    main()
