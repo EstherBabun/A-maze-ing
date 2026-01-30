2ce2b3b79f3885c12a912752d5ce27c091c4061b
Morgane <meurer.morgane@yahoo.fr>
2026-01-30 23:29:20 +0100

    draft

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -7,7 +7,6 @@
 
 import sys
 from maze_generator import MazeGenerator
-from typing import List
 
 """
 Entry point of the A-maze-ing program.
@@ -17,32 +16,6 @@ appropriate maze renderer based on the configuration file.
 """
 
 
-def check_display(config_file: str) -> str:
-    """
-    Read the configuration file and extract the display mode.
-
-    Args:
-        config_file (str): Path to the configuration file.
-
-    Returns:
-        str: the whole string wrote by the user.
-    """
-    content: List[str] = []
-    try:
-        with open(config_file, "r") as f:
-            content = f.readlines()
-    except Exception as e:
-        return f"Error: {e}"
-    display: str = ""
-    for line in content:
-        line_upper: str = line.upper()
-        if "DISPLAY" in line_upper:
-            parts: List[str] = line_upper.split("=")
-            if len(parts) > 1:
-                display = parts[1].strip()
-    return display
-
-
 def main() -> None:
     """
     Parse command-line arguments and launch the maze renderer.
@@ -50,23 +23,16 @@ def main() -> None:
     This function selects the appropriate renderer based on the
     configuration file and starts the maze display.
     """
+    config = ""
+    if sys.argv[1]:
+    	config += sys.argv[1]
+    maze: MazeGenerator = MazeGenerator(config)
     if len(sys.argv) == 1:
         print(1)
         # renderer = MazeRenderer()
     elif len(sys.argv) == 2:
-        config_file: str = sys.argv[1]
-        display: str = check_display(config_file)
-        maze: MazeGenerator = MazeGenerator(config_file)
         maze.generate_maze()
         maze.display_maze()
-
-        # peut etre mis dans la classe directement et du coup plus besoin de la première fonction
-        if display == "MLX":
-            print("mlx")
-            # renderer = MazeRenderer(config_file)
-        else:
-            maze.display_maze()
-
     else:
         print("Usage: python3 a_maze_ing.py config_file(optional)")
         return
